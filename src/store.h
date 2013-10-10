d18fe6bf61fabb9a006d0e72469beec0471a5a38
Jeff Abrahamson <jeff@purple.com>
2013-10-10 01:03:57 +0100

    Add store.h, the interface for the data store.

---
+++ src/store.h
@@ -0,0 +1,44 @@
+/*
+  Copyright 2013  Jeff Abrahamson
+  
+  This file is part of tsm.
+  
+  tsm is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  tsm is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with tsm.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+#ifndef __STORE_H__
+#define __STORE_H__ 1
+
+namespace tsm {
+
+        class Database;
+        class Series;
+
+        class Store {
+                // Takes ownership of the database pointer.
+                Store(Database *db);
+                ~Store();
+
+                // Can throw StorageError;
+                template<typename T> void set(T when, double value);
+                // Can throw OutofTemporalRange, StorageError.
+                template<typename T> double get(T when);
+                template<typename T> Series get(T begin, T end);
+        };
+
+} /* namespace tsm */
+
+#endif  /* __STORE_H__*/
